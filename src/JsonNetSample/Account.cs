ebe94995fae351893214968488b318bc3c11136b
Takayuki Kondo <tkondou@gmail.com>
2020-06-21 17:27:18 +0900

    :shower: refactoring

---
+++ src/JsonNetSample/Account.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonNetSample
+{
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+
+        public static Account GetInstanceFromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Account>(json);
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+
+    public static class SerializeDerializeObject
+    {
+        /// <summary>
+        /// シリアライズ(Accountオブジェクトを json に変換)
+        /// </summary>
+        public static void Serialize()
+        {
+            Account account = new Account
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                {
+                    "User",
+                    "Admin"
+                }
+            };
+
+            string json = account.ToJson();
+            Console.WriteLine(json);
+        }
+
+        /// <summary>
+        /// デシリアライズ (json を Accountオブジェクトに変換)
+        /// </summary>
+        public static void Deserialize()
+        {
+            string json = @"{
+  'Email': 'james@example.com',
+  'Active': true,
+  'CreatedDate': '2013-01-20T00:00:00Z',
+  'Roles': [
+    'User',
+    'Admin'
+  ]
+}";
+            var account = Account.GetInstanceFromJson(json);
+            Console.WriteLine(account.Email);
+            Console.WriteLine(account.Active);
+            Console.WriteLine(account.CreatedDate);
+        }
+    }
+}
--- src/JsonNetSample/Program.cs
+++ src/JsonNetSample/Program.cs
@@ -1,64 +1,14 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace JsonNetSample
 {
-    public class Account
-    {
-        public string Email { get; set; }
-        public bool Active { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public IList<string> Roles { get; set; }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            // シリアライズ (Accountオブジェクトを json に変換)
-            Account account = new Account
-            {
-                Email = "james@example.com",
-                Active = true,
-                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
-                Roles = new List<string>
-                {
-                    "User",
-                    "Admin"
-                }
-            };
-
-            string json = SerializeAccount(account);
-            Console.WriteLine(json);
-
-
-            // デシリアライズ (json を Accountオブジェクトに変換)
-            json = @"{
-  'Email': 'james@example.com',
-  'Active': true,
-  'CreatedDate': '2013-01-20T00:00:00Z',
-  'Roles': [
-    'User',
-    'Admin'
-  ]
-}";
-            account = DeserializeAccount(json);
-            Console.WriteLine(account.Email);
+            SerializeDerializeObject.Serialize();
+            SerializeDerializeObject.Deserialize();
         }
-
-        static string SerializeAccount(Account account)
-        {
-            return JsonConvert.SerializeObject(account, Formatting.Indented);
-        }
-
-        static Account DeserializeAccount(string json)
-        {
-            return JsonConvert.DeserializeObject<Account>(json);
-        }
-
     }
-
-
-
-
 }
